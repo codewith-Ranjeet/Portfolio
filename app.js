aea2f03625c9949a9f55f3734943b5851462db81
codewith-Ranjeet <ranjeetshinde2212@gmail.com>
2025-07-09 11:01:45 +0530

    Added Email pop-up

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+// hamburger menu
 const burger = document.createElement('div');
 
 burger.className = 'hamburger';
 burger.innerHTML = '<i class="fas fa-bars"></i>';
 document.querySelector('.navbar').appendChild(burger);
 burger.addEventListener('click', () => document.querySelector('.nav-elements').classList.toggle('active'));
+
+// Email pop-up
+function openForm() {
+    document.getElementById("emailForm").classList.add("active");
+}
+
+function closeForm() {
+    document.getElementById("emailForm").classList.remove("active");
+}
